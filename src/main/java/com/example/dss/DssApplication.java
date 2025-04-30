5eff7997d3ceaa6569157648bfecbea510cccb0f
unknown <marvin.pire@student.hepl.be>
2025-04-30 14:18:52 +0200

    refactor

--- src/main/java/com/example/dss/DssApplication.java
+++ src/main/java/com/example/dss/DssApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DssApplication {
+	public DssApplication() {
+		System.out.println("test");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DssApplication.class, args);
